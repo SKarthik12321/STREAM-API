a38be6e9cbdd7b217c9ab14a55cec94560007720
S KARTHIK <skarthik.link@gmail.com>
2026-02-12 11:11:40 +0530

    UC2.7 summary statistics

--- src/main/java/com/streamapi/StreamAPIUseCases.java
+++ src/main/java/com/streamapi/StreamAPIUseCases.java
@@ -1,15 +1,17 @@
 package com.streamapi;
 import java.util.*;
+import java.util.stream.*;
 
 public class StreamAPIUseCases {
     public static void main(String[] args) {
 
-        List<Integer> nums = Arrays.asList(5,2,9,1,7);
+        IntSummaryStatistics stats =
+            Arrays.asList(2,4,6,8,10)
+                  .stream()
+                  .mapToInt(Integer::intValue)
+                  .summaryStatistics();
 
-        int min = nums.stream().min(Integer::compare).get();
-        int max = nums.stream().max(Integer::compare).get();
-
-        System.out.println("Min = " + min);
-        System.out.println("Max = " + max);
+        System.out.println("Sum = " + stats.getSum());
+        System.out.println("Average = " + stats.getAverage());
     }
 }
