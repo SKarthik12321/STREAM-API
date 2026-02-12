612ddf76a1031c74ba894b753f423155b9dfeef8
S KARTHIK <skarthik.link@gmail.com>
2026-02-12 11:15:29 +0530

    UC2.9 sorted

--- src/main/java/com/streamapi/StreamAPIUseCases.java
+++ src/main/java/com/streamapi/StreamAPIUseCases.java
@@ -1,15 +1,15 @@
 package com.streamapi;
 import java.util.*;
+import java.util.stream.*;
 
 public class StreamAPIUseCases {
     public static void main(String[] args) {
 
-        List<Integer> nums = Arrays.asList(2,4,6,8,9);
-
-        boolean allEven = nums.stream().allMatch(n -> n % 2 == 0);
-        boolean anyEven = nums.stream().anyMatch(n -> n % 2 == 0);
-
-        System.out.println("All even = " + allEven);
-        System.out.println("Any even = " + anyEven);
+        System.out.println(
+            Arrays.asList(9,3,7,1,5)
+                  .stream()
+                  .sorted()
+                  .collect(Collectors.toList())
+        );
     }
 }
