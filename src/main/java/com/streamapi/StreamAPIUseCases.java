96e2283708d51e6c7b14f206db59523d215a1de5
S KARTHIK <skarthik.link@gmail.com>
2026-02-12 11:02:36 +0530

    UC2.3 collect

--- src/main/java/com/streamapi/StreamAPIUseCases.java
+++ src/main/java/com/streamapi/StreamAPIUseCases.java
@@ -5,11 +5,11 @@ import java.util.stream.*;
 public class StreamAPIUseCases {
     public static void main(String[] args) {
 
-        System.out.println(
-            Arrays.asList(1,2,3,4,5)
+        List<Integer> result =
+            Arrays.asList(2,4,6,8)
                   .stream()
-                  .map(n -> n * 2)
-                  .collect(Collectors.toList())
-        );
+                  .collect(Collectors.toList());
+
+        System.out.println(result);
     }
 }
